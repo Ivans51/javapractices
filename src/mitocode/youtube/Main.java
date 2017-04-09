a9d6a0aa4c517f729e2eacddc8f5384d9374fad4
Ivans51 <Ivans1041@gmail.com>
2017-04-09 15:36:37 -0400

    Data Types

---
+++ src/mitocode/youtube/Main.java
@@ -0,0 +1,21 @@
+package mitocode.youtube;
+
+import mitocode.youtube.basic.DataType;
+
+/**
+ * Created by Ivans on 08/04/2017.
+ * Tipos de datos en Java
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        DataType dataType = new DataType();
+
+        // Character escape
+        System.out.println("Hoal Ivans \neste es el curso de java");
+        System.out.println("Una instancia de un atribuo " + dataType.result);
+
+    }
+
+}
