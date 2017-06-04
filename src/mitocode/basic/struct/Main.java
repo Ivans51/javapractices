c988e79ab3090605a260dbde23802a04c9c604c6
Ivans51 <Ivans1041@gmail.com>
2017-06-04 11:08:00 -0400

    Local

--- src/mitocode/basic/struct/Main.java
+++ src/mitocode/basic/struct/Main.java
@@ -7,14 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Instance
+        // Instance new
         StructSelection structSelection = new StructSelection();
         StructWhile structWhile = new StructWhile();
         StructFor structFor = new StructFor();
         StructDoWhile structDoWhile = new StructDoWhile();
         StructSwitch structSwitch = new StructSwitch();
 
-        // Message in console
+        // Message in console new Changes 2
+        // Commmit local 1
         System.out.println("Selección Simple " + structSelection.selectionSingle(5, 10));
         System.out.println("Operador ternario" + structSelection.operadorTernario(10, 5));
 
